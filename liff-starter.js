8cb71cd832370d78e3480a88d5d61d27473da419
Sirinan Jirinsa <34699997+sirinanjirinsa@users.noreply.github.com>
2018-12-06 15:00:59 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,25 +1,15 @@
 window.onload = function (e) {
     liff.init(function (data) {
+        
         liff.getProfile().then(function (profile) {
             var profilePictureDiv = document.getElementById('profilepicturediv');
             var img = document.createElement('img');
             img.src = profile.pictureUrl;
-            img.alt = "Profile Picture";
             profilePictureDiv.appendChild(img);
         }).catch(function (error) {
             window.alert("Error getting profile: " + error);
         });
-        initializeApp(data);
+        
     });
    
 };
-
-function initializeApp(data) {
-    document.getElementById('languagefield').textContent = data.language;
-    document.getElementById('viewtypefield').textContent = data.context.viewType;
-    document.getElementById('useridfield').textContent = data.context.userId;
-    document.getElementById('utouidfield').textContent = data.context.utouId;
-    document.getElementById('roomidfield').textContent = data.context.roomId;
-    document.getElementById('groupidfield').textContent = data.context.groupId;
-   
-}
