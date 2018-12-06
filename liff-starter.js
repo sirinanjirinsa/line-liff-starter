08f3a568f8ef04e1166b3cf73e7dfd1525d569e5
Sirinan Jirinsa <34699997+sirinanjirinsa@users.noreply.github.com>
2018-12-06 15:59:09 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -11,8 +11,23 @@ window.onload = function (e) {
             window.alert("Error getting profile: " + error);
         });
         
-        
-        
+        document.getElementById('sendmessagebutton').addEventListener('click', function () {
+        liff.sendMessages([{
+            type: 'text',
+            text: "You've successfully sent a message! Hooray!"
+        }, {
+            type: 'sticker',
+            packageId: '2',
+            stickerId: '144'
+        }]).then(function () {
+            window.alert("Message sent");
+        }).catch(function (error) {
+            window.alert("Error sending message: " + error);
+        });
+            
+            
+    });
+   
     });
    
 };
